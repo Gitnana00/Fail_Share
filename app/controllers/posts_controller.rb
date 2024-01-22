fbff043743c90b667f1048a43b3205e5afce5651
Gitnana00 <0818uzimihs@gmail.com>
2024-01-22 20:56:34 +0900

    🌟投稿削除機能を実装しました

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -39,7 +39,7 @@ class PostsController < ApplicationController
   def destroy
     post = Post.find(params[:id])
     post.destroy!
-    redirect_to posts_url, notice: '削除完了だよ！'
+    redirect_to posts_path, notice: '削除完了だよ！', status: :see_other
   end
 
   private
