d24c7e1f8e160cda66f3d62b3c416fe2276e7d9f
Gitnana00 <0818uzimihs@gmail.com>
2024-02-06 03:19:56 +0900

    🌟add/autocomplete

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,11 @@ Rails.application.routes.draw do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
 
-  resources :posts, only: [:index, :new, :create, :show, :edit, :update, :destroy]
+  resources :posts, only: [:index, :new, :create, :show, :edit, :update, :destroy] do
+    collection do
+      get :search
+    end
+  end
 
   get '/terms', to: 'static_pages#terms'
   get '/privacy', to: 'static_pages#privacy'
