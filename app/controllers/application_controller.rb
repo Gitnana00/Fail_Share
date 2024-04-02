24047cd822b74b5d754d63a87c751bb49ca39ba2
Gitnana00 <0818uzimihs@gmail.com>
2024-04-02 09:52:34 +0900

    Change display from email to name in posts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,14 @@
-# frozen_string_literal: true
-
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 
   def after_sign_out_path_for(_resource_or_scope)
     new_user_session_path
   end
-
 end
